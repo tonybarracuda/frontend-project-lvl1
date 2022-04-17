9ed8bf28ce51846814c6aa1b8770e5c17b436a18
Anton Bar <tonybarracuda@list.ru>
2022-04-17 13:47:37 +0300

    edit bin/brain-even.js game logic

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,19 +2,30 @@
 
 import readlineSync from 'readline-sync';
 
+const genRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const isEven = (number) => number % 2 === 0;
+
 const startGame = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const firstNum = 15;
-  const firstQ = readlineSync.question(`Question: ${firstNum} \nYour answer: `);
-  if (firstQ === 'no') {
+
+  for (let i = 0; i < 3; i += 1) {
+    const number = genRandomNumber(0, 100);
+    const userAnswer = readlineSync.question(
+      `Question: ${number} \nYour answer: `,
+    );
+    const correctAnswer = isEven(number) ? 'yes' : 'no';
+    if (userAnswer !== correctAnswer) {
+      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+      console.log(`Let's try again, ${userName}`);
+      return;
+    }
     console.log('Correct!');
-  } else {
-    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-    console.log(`Let's try again, ${userName}`);
+
+    console.log(`Congradulations, ${userName}!`);
   }
 };
 
